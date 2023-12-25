07f80dc8a1cabf7e67533ff7723e4c9d3014df86
Hariom Singh <hariomhks1997@gmail.com>
2023-12-25 12:43:00 +0530

    2 Integrating CRUD with MongoDB

--- util/database.js
+++ util/database.js
@@ -2,12 +2,10 @@ const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
 let _db;
-console.log(process.env.MONGODB_USERNAME)
-console.log(process.env.MONGODB_PASSWORD)
 
 const mongoConnect = callback => {
   MongoClient.connect(
-    `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.6sujnmk.mongodb.net/?retryWrites=true&w=majority`
+    'mongodb+srv://hariomhks088:987654321@cluster0.6sujnmk.mongodb.net/?retryWrites=true&w=majority'
   )
     .then(client => {
       console.log('Connected!');
@@ -29,4 +27,3 @@ const getDb = () => {
 
 exports.mongoConnect = mongoConnect;
 exports.getDb = getDb;
- 
